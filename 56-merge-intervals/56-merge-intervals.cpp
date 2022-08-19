6053cf1e5f3145a115596973d8ce9b43808f7fb2
Shreyas Kulkarni <80305828+Shreyass48@users.noreply.github.com>
2022-08-19 11:57:31 +0530

    Time: 51 ms (71.09%), Space: 18.9 MB (94.54%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -2,53 +2,53 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) 
     {
-//         vector<vector<int>>ans;
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end());
+        // set strt ans end, start as first ele and end as last ele in row
+        int strt = intervals[0][0];
+        int end = intervals[0][1];
         
-//         // set strt ans end, start as first ele and end as last ele in row
-//         int strt = intervals[0][0];
-//         int end = intervals[0][1];
-        
-//         for(int i = 1; i < intervals.size(); i++)
-//         {
-//             // compairing values
-//             //intervals[i][0] is current value
-//             // strt and end is prev values
-//             if(intervals[i][0] <= end)
-//             {
-//                 strt = min(intervals[i][0],strt);
-//                 end = max(intervals[i][1],end);
-//             }
-//             else
-//             {
-//                 ans.push_back({strt,end});
-//                 strt = intervals[i][0];
-//                 end  = intervals[i][1];
-//             }
-//         }
-        
-//         // for last record, last strt end
-//         ans.push_back({strt,end});
-        
-//         return ans;
-        vector<vector<int>> v;
-        sort(intervals.begin(), intervals.end());
-        int n = intervals.size(), x = intervals[0][0], y = intervals[0][1];
-        for(int i = 1 ; i<n ; i++)
+        for(int i = 1; i < intervals.size(); i++)
         {
-            if(intervals[i][0]>=x && intervals[i][0]<=y)
+            // compairing values
+            //intervals[i][0] is current value
+            // strt and end is prev values
+            if(intervals[i][0] <= end)
             {
-                x = min(intervals[i][0], x);
-                y = max(intervals[i][1], y);
+                strt = min(intervals[i][0],strt);
+                end = max(intervals[i][1],end);
             }
             else
             {
-                v.push_back({x,y});
-                x = intervals[i][0];
-                y = intervals[i][1];
+                ans.push_back({strt,end});
+                strt = intervals[i][0];
+                end  = intervals[i][1];
             }
         }
-        v.push_back({x,y});
-        return v;
+        
+        // for last record, last strt end
+        ans.push_back({strt,end});
+        
+        return ans;
+        // vector<vector<int>> v;
+        // sort(intervals.begin(), intervals.end());
+        // int n = intervals.size(), x = intervals[0][0], y = intervals[0][1];
+        // for(int i = 1 ; i<n ; i++)
+        // {
+        //     if(intervals[i][0]>=x && intervals[i][0]<=y)
+        //     {
+        //         x = min(intervals[i][0], x);
+        //         y = max(intervals[i][1], y);
+        //     }
+        //     else
+        //     {
+        //         v.push_back({x,y});
+        //         x = intervals[i][0];
+        //         y = intervals[i][1];
+        //     }
+        // }
+        // v.push_back({x,y});
+        // return v;
 
     }
 };
